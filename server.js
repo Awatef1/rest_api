8925ab2a7991fc51015e6e2447f34b2db1335815
Awatef1 <84491937+Awatef1@users.noreply.github.com>
2021-09-09 22:02:50 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express")
+require("dotenv").config()
+const connectDB =require("./config/connectDB")
+connectDB()
+const app =express();
+app.use(express.json())
+app.use("/api/user",require("./routes/User"))
+
+
+const PORT = process.env.PORT || 6000;
+
+app.listen(PORT,(err)=>{
+    err? console.error(err) : console.log(`server is ruuning in PORT ${PORT}`)
+})
